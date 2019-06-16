7390d93ee9011316a3fd9de04784dd46ec05a59f
martin-utbm <48589251+martin-utbm@users.noreply.github.com>
2019-06-16 17:01:36 +0200

    Update Equipement.java

--- src/main/java/com/gl52/gestappmedv2/Entities/Equipement.java
+++ src/main/java/com/gl52/gestappmedv2/Entities/Equipement.java
@@ -3,42 +3,30 @@ package com.gl52.gestappmedv2.Entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Collection;
-
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Equipement implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String lastKnowsLocation;
     private Boolean enMaintenance;
     private Boolean enPret;
 
-    //Cardinalité Equipement-Type
     @ManyToOne
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Type type;
 
-    //cardinalité Equipement-Pret
-    @OneToMany(mappedBy = "equipement")
-    Collection<Pret> prets;
+    @OneToMany(mappedBy = "equipement", cascade = CascadeType.REMOVE)
+    private Collection<Pret> prets;
 
-    //cardinalité Equipement-Maintenance
-    @OneToMany(mappedBy = "equipement")
-    Collection<Maintenance> maintenances;
-
-
+    @OneToMany(mappedBy = "equipement", cascade = CascadeType.REMOVE)
+    private Collection<Maintenance> maintenances;
 }
-
-
