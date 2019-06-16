535f9679264da7fba2badd92a757510e32ca7090
martin-utbm <48589251+martin-utbm@users.noreply.github.com>
2019-06-16 17:01:13 +0200

    Update Pret.java

--- src/main/java/com/gl52/gestappmedv2/Entities/Pret.java
+++ src/main/java/com/gl52/gestappmedv2/Entities/Pret.java
@@ -1,12 +1,7 @@
 package com.gl52.gestappmedv2.Entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
@@ -14,25 +9,19 @@ import java.util.Date;
 @Entity
 @Data
 @NoArgsConstructor
-
-
 public class Pret implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Date debut;
-    private Date fin;
-    @Column(name = "Etat_De_Debut", columnDefinition = "enum('UNUSABLE','DEGRADED','GOOD','EXCELLENT')")
-    private String etatDebut;
-    @Column(name = "Etat_De_Fin", columnDefinition = "enum('UNUSABLE','DEGRADED','GOOD','EXCELLENT')")
-    private String etatFin;
+
+    private Date start;
+    private Date end;
+    private int startWear;
+    private int endWear;
 
     @ManyToOne
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Equipement equipement;
+
     @ManyToOne
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Client client;
-
-
 }
