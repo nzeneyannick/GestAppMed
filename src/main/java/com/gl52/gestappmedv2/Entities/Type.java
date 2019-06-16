87788ed9b3ea198b846b16dc93a6d843a3e331ac
Yannick DJOPGANG <nzeneyannick@yahoo.fr>
2019-06-16 13:42:33 +0200

    ajout du champ Type/maintenanceInterval

--- src/main/java/com/gl52/gestappmedv2/Entities/Type.java
+++ src/main/java/com/gl52/gestappmedv2/Entities/Type.java
@@ -17,6 +17,7 @@ public class Type implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String maintenanceInterval;
     //Un type comporte plusieurs equipements
     @OneToMany(mappedBy = "type")
     private Collection<Equipement> equipements;
